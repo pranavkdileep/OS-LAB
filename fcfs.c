01225b7fe51a73a948f409dfb3c66e33bce67792
Your Name <you@example.com>
2024-04-03 19:38:38 +0530

    first commit

---
+++ fcfs.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,TAT[10],WT[10],AT[10],BT[10],CT[10],totlalTAT=0,totalWT=0;
+    float avgTAT=0,avgWT=0;
+    printf("Enter the number of processes: ");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        printf("Enter the arrival time of process %d: ",i+1);
+        scanf("%d",&AT[i]);
+        printf("Enter the burst time of process %d: ",i+1);
+        scanf("%d",&BT[i]);
+    }
+    CT[0]=AT[0]+BT[0];
+    for(int i=1;i<n;i++)
+    {
+        CT[i]=CT[i-1]+BT[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        TAT[i]=CT[i]-AT[i];
+        totlalTAT+=TAT[i];
+        WT[i]=TAT[i]-BT[i];
+        totalWT+=WT[i];
+    }
+    avgTAT=(float)totlalTAT/n;
+    avgWT=(float)totalWT/n;
+    printf("Process\tAT\tBT\tCT\tTAT\tWT\n");
+    for(int i=0;i<n;i++)
+    {
+        printf("P%d\t%d\t%d\t%d\t%d\t%d\n",i+1,AT[i],BT[i],CT[i],TAT[i],WT[i]);
+    }
+    printf("Average Turn Around Time: %f\n",avgTAT);
+    printf("Average Waiting Time: %f\n",avgWT);
+    return 0;
+}
